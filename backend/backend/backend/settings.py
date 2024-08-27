97b0c7178763cef297459f3dfd3ea4c73828148e
Abhimanyu Sharma <53882305+Abhi712-ui@users.noreply.github.com>
2024-08-27 12:47:34 -0500

    no more vercel

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z2e_)grysc2p*xud000#3i5_3@#-s%=mb*jhkegyw@rsnxs)a8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['abhimanyu-sharma-backend.netlify.app']
+ALLOWED_HOSTS = ['*']
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
